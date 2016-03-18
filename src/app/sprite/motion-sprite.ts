d4dc6c9906fa8ac44900147c9df4ab816d2ad7f0
digital-synapse <digital.synapse.software@gmail.com>
2016-03-18 04:37:03 -0700

    no message

--- src/app/sprite/motion-sprite.ts
+++ src/app/sprite/motion-sprite.ts
@@ -1,38 +1,66 @@
-/// <reference path="./ghost-sprite.ts"/>
-/// <reference path="../game.ts"/>
+// <reference path="../game.ts"/>
 /// <reference path="../random.ts"/>
 
-class MotionSprite extends GhostSprite {
+const PI2 = Math.PI*2;
+class MotionSprite extends Phaser.Sprite {
  
     constructor(){                
         var x= Random.int(Game.instance.width);
         var y= Random.int(Game.instance.height);
         var game = Game.instance;
-        super(game,x,y,'progranism', 10, 0.5);
+        super(game,x,y,'progranism');
+        this.scale = new Phaser.Point(0.5,0.5);
+        game.add.existing(this);
+        game.physics.arcade.enable(this);
         this.setTarget(Random.int(game.width), Random.int(game.height));
+        this.rotation = Random.double(2*Math.PI);
+        this.velocity = Random.double(50);
+        this.turnRadius = 1/this.velocity;         
     }
+    
+    public turnRadius:number;
+    public velocity:number;
+    
+    private pointFromPolar(magnitude:number, angle:number, degrees:boolean = false) {
+        return new Phaser.Point(1,0).setMagnitude(magnitude).rotate(0,0,angle,degrees);
+    }
+    
+    private getAngleFromPoint( firstPoint:Phaser.Point, secondPoint:Phaser.Point):number {
+
+        if((secondPoint.x > firstPoint.x)) {//above 0 to 180 degrees
+
+            return Math.atan2((secondPoint.x - firstPoint.x), (firstPoint.y - secondPoint.y));
+
+        }
+        else if((secondPoint.x < firstPoint.x)) {//above 180 degrees to 360/0
+
+            return  PI2- Math.atan2((firstPoint.x - secondPoint.x), (firstPoint.y - secondPoint.y));
+
+        }//End if((secondPoint.x > firstPoint.x) && (secondPoint.y <= firstPoint.y))
+
+        return Math.atan2(0 ,0);
+
+    }    
     
     public setTarget(x:number, y:number){
         if (!this.hasTarget){
-            this.targetX=x;
-            this.targetY=y;
+            this.target = new Phaser.Point(x,y);
             this.hasTarget=true;
         }
     }    
     private hasTarget: boolean;
-    private targetX:number;
-    private targetY:number;
+    private target:Phaser.Point;
+    public body: Phaser.Physics.Arcade.Body;
     
     public update(){
         
-        var x = Random.int(this.targetX-150, this.targetX+150);
-        var y = Random.int(this.targetY-150, this.targetY+150);
-        this.game.physics.arcade.moveToXY(this, x,y,250);
-        if (this.hasTarget 
-            && Math.abs(x-this.position.x) < 300 
-            && Math.abs(y-this.position.y) < 300) 
-            this.hasTarget=false;
-            
-        super.update();
+        var angle = this.getAngleFromPoint(this.position, this.target);
+        if ((angle -this.rotation) < 0){
+            this.rotation -= this.turnRadius;
+        }
+        else {
+            this.rotation += this.turnRadius;
+        }
+        this.body.velocity= this.pointFromPolar(this.velocity,this.rotation);
     }        
 }
